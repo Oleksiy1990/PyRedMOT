21e9c7949508b0ca0ecf32cc70cc382e63d4c3da
Oleksiy1990 <oleksiy1990@gmail.com>
2016-07-14 18:46:16 +0200

    Errors in beam descriptions!
    The Gaussian beams in x,y,z directions are apparenetly wrong. They must
    be written in 2D because the saturation intensity is by definition a 2D
    measure, and plus the position dependence of the intensity must be
    written correctly for each axis

--- blueMOT3D_new.py
+++ blueMOT3D_new.py
@@ -40,7 +40,8 @@ def diffeqs_blue(variables,t,params): #We disregard gravity
 
     derivs = [vx,(1/mSr88)*MOT_force(kVecBlue,blueGamma,gaussian_Xbeam/blueIsat,vx,B_gradient,x,detunings_list),\
             vy,(1/mSr88)*MOT_force(kVecBlue,blueGamma,gaussian_Ybeam/blueIsat,vy,B_gradient,y,detunings_list),\
-            vz,(1/mSr88)*MOT_force(kVecBlue,blueGamma,gaussian_Zbeam/blueIsat,vz,-2*B_gradient,z,detunings_list)]
+            vz,(1/mSr88)*MOT_force(kVecBlue,blueGamma,gaussian_Zbeam/blueIsat,vz,2*B_gradient,z,detunings_list)]
+            #One needs to be careful with the sign in front of 2B_gradient in the z_dir
 
     # Quadrupole field from: 
     # https://www2.physics.ox.ac.uk/sites/default/files/2013-01-19/minsung_pdf_16672.pdf eq. 2.40
@@ -56,15 +57,19 @@ detunings_blue = [-blueGamma]
 # num_redCombLines = 50
 # detunings_red = np.linspace(-2*np.pi*200*10**3,-2*np.pi*5*10**6,num_redCombLines)
 
-bluePowerX = bluePowerY = 10*10**-3 
-bluePowerZ = 10*10**-3
-blueRadX = blueRadY = 10*10**-3
-blueRadZ = 10*10**-3
-blueGradient = 0.55 # T/m = 55 G/cm
+
+#Put only integer number of milliwatts, not fractions
+bluePowerX = bluePowerY = 15*10**-3 
+bluePowerZ = 2*10**-3
+#Put only integer number of millimeters, not fractions
+# blueRadX = blueRadY = 10*10**-3
+# blueRadZ = 10*10**-3
+blueGradientGcm = 55 #put an integer number here
+blueGradient = 0.01*blueGradientGcm # T/m = 55 G/cm
 
 
 
-parameters_blue = [bluePowerX,bluePowerY,bluePowerZ,blueRadX,blueRadY,blueRadZ,blueGradient,detunings_blue]
+# parameters_blue = [bluePowerX,bluePowerY,bluePowerZ,blueRadX,blueRadY,blueRadZ,blueGradient,detunings_blue]
 
 
 
@@ -72,7 +77,7 @@ tStop = 0.5
 t = np.linspace(0., tStop, 10**5)
 
 initz = np.linspace(-12e-3,12e-3,7)
-init_rad = 0.05
+init_rad = 0.1
 init_angle = 30*(np.pi/180) #not to change
 x_shifts = np.linspace(0,13.86e-3,5)
 y_shifts = np.linspace(0,24e-3,5)
@@ -83,7 +88,7 @@ inits_pos_2 = np.array(list(itertools.product([initx[0]],inity[1:],initz)))
 inits_pos = np.concatenate((inits_pos_1,inits_pos_2))
 
 
-init_speed_xy = 30 #m/s
+init_speed_xy = 40 #m/s
 vel_angle = np.linspace(25,35,5) #deg
 initvx = init_speed_xy*np.sin(vel_angle*np.pi/180)
 initvy = init_speed_xy*np.cos(vel_angle*np.pi/180)
@@ -92,110 +97,7 @@ inits_vel = np.array([[init_speed_xy*np.sin(angle*np.pi/180),init_speed_xy*np.co
 
 
 inits = np.array([[p[0],v[0],p[1],v[1],p[2],v[2]] for p in inits_pos for v in inits_vel])
-print(inits.shape)
 
-
-
-final_pos_captured = []
-final_pos_lost = []
-weird = []
-
-for num,ic in enumerate(inits):
-	print("Solving ",num)
-	solution_blue = odeint(diffeqs_blue, ic, t, args=(parameters_blue,),mxstep=10**8)
-	fp = np.sqrt(solution_blue[-1,0]**2+solution_blue[-1,2]**2+solution_blue[-1,4]**2)
-	if fp<=1e-4:
-		final_pos_captured.append(fp)
-	elif fp>1e-4:
-		final_pos_lost.append(fp)
-	else:
-		weird.append(fp)
-
-print(len(final_pos_captured))
-print(len(final_pos_lost))
-print(len(weird))
-
-sys.exit(0)
-
-position_res = np.sqrt(solution_blue[:,0]**2+solution_blue[:,2]**2+solution_blue[:,4]**2)
-
-# Plot results
-fig = plt.figure(1, figsize=(8,8))
-
-# Plot theta as a function of time
-ax1 = fig.add_subplot(111)
-ax1.scatter(solution_blue[:,0],solution_blue[:,2])
-ax1.set_xlabel('x')
-ax1.set_ylabel('y')
-plt.show()
-
-
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# scat = ax.scatter(xx,yy,zz)
-# ax.set_xlabel('X Label')
-# ax.set_ylabel('Y Label')
-# ax.set_zlabel('Z Label')
-
-# plt.show()
-
-# sys.exit(0)
-
-vel_pts = 5
-#vel_min = 0.5
-vel_rad = 3 # m/s
-vel_cube_x = np.linspace(-vel_rad,vel_rad,vel_pts)
-vel_cube_y = np.linspace(-vel_rad,vel_rad,vel_pts)
-vel_cube_z = np.linspace(-vel_rad,vel_rad,vel_pts)
-
-
-vx,vy,vz = np.meshgrid(vel_cube_x,vel_cube_y,vel_cube_z,indexing="ij")
-vxf = vx.flatten()
-vyf = vy.flatten()
-vzf = vz.flatten()
-total_velocities = np.column_stack((vxf,vyf,vzf))
-total_velocities_ball = np.array([q for q in total_velocities if (q[0]**2+q[1]**2+q[2]**2) <= vel_rad**2])
-
-
-# print(total_velocities_ball)
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# #scat = ax.scatter(vx,vy,vz)
-# scat = ax.scatter(total_velocities_ball[:,0],total_velocities_ball[:,1],total_velocities_ball[:,2])
-# ax.set_xlabel('X Label')
-# ax.set_ylabel('Y Label')
-# ax.set_zlabel('Z Label')
-
-# plt.show()
-
-# sys.exit(0)
-
-
-
-
-## Let's leave the random sampling of velocities for now
-# vel_pts = 50 # number of random starting velocities to check
-# vel_rad = 5 # the max speed which will be accepted
-# vel_cube = uniform(low=-vel_rad,high=vel_rad,size=(vel_pts,1,3))
-# vel_ball = np.array([q for q in vel_cube if (q[:,0]**2+q[:,1]**2+q[:,2]**2) <= vel_rad**2])
-
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(pos_ball[:,:,0], pos_ball[:,:,1], pos_ball[:,:,2])
-# ax.set_xlabel('X Label')
-# ax.set_ylabel('Y Label')
-# ax.set_zlabel('Z Label')
-
-# plt.show()
-
-#initial_red = [(q[:,0],q[:,1],q[:,2]) for q in pos_ball]
-#initial_red = np.array([(q[:,0],v[:,0],q[:,1],v[:,1],q[:,2],v[:,2]) for q in pos_ball for v in vel_ball])
-
-
-initialconds_red = [(q[0],v[0],q[1],v[0],q[2],v[0]) for q in total_positions for v in total_velocities_ball]
-
-# print(len(initialconds_red))
-# sys.exit(0)
 
 class Results(tables.IsDescription):
     x_init = tables.Float64Col()   
@@ -212,67 +114,72 @@ class Results(tables.IsDescription):
     vz_final = tables.Float64Col()
    
 
-class Detunings(tables.IsDescription):
-    detun = tables.Float64Col()
-    laserPowerX = tables.Float64Col()
-    laserPowerY = tables.Float64Col()
-    laserPowerZ = tables.Float64Col() 
-    beamWaistRadX = tables.Float64Col()
-    beamWaistRadY = tables.Float64Col()
-    beamWaistRadZ = tables.Float64Col()
-
-file_save = tables.open_file("C:/Users/Oleksiy/Desktop/SimulationResults/redMOT/UnifPowerUnifWaistComb%.i.hdf5"%num_redCombLines,mode="a",title= "Red MOT simulation, %.i comb"%num_redCombLines)
-grp_grad = file_save.create_group("/","grad%.3f"%(redGradient*100),title="Red gradient: %.3f G/cm"%(redGradient*100))
-
-grp_simulation = file_save.create_group(grp_grad,str(int(redPowerXtotal*10**3))+"_"+str(int(redRadX*10**3)),\
-    title="Power: " + str(int(redPowerXtotal*10**3))+ " mW, beam rad.:" +str(int(redRadX*10**3))+" mm")
-
-grp_detunings = file_save.create_group(grp_simulation,"detuningsHz",\
-    title="Values of freqs of the comb lines, in Hz, red-detuned from resonance")
-tbl_detunings = file_save.create_table(grp_detunings,"detunings",Detunings)
-detunigns_save = tbl_detunings.row
-for i in range(len(detunings_red)):
-    detunigns_save["detun"] = detunings_red[i]
-    detunigns_save.append()
-tbl_detunings.flush()
+class Descriptions(tables.IsDescription):
+    detuning = tables.Float64Col()
+    gradient = tables.Float64Col()
+    init_position_away = tables.Float64Col()
+    PowerX = tables.Float64Col()
+    PowerY = tables.Float64Col()
+    PowerZ = tables.Float64Col() 
+    WaistRadX = tables.Float64Col()
+    WaistRadY = tables.Float64Col()
+    WaistRadZ = tables.Float64Col()
 
 
 
-print("Getting ready to run the solution loop")
+# grp_descriptions = file_save.create_group(grp_simulation,"Descriptions",\
+#     title="The input parameters of the simulation are given here, units are SI")
 
-counter = 0
+radiiXY = np.array([2,7,12,17])*1e-3
+for radX in radiiXY:
+	radY = radZ = radX
+	parameters_blue = [bluePowerX,bluePowerY,bluePowerZ,radX,radY,radZ,blueGradient,detunings_blue]
 
-for inits in initialconds_red: 
+	file_save = tables.open_file("resultsBlueMOT/pX%.ipY%.ipZ%.igrad%.i.hdf5"%(int(bluePowerX*1e3),int(bluePowerY*1e3),int(bluePowerZ*1e3),\
+		blueGradientGcm),mode="a",title= "Blue MOT simulation, detuning -1*gamma")
+	grp_simulation = file_save.create_group("/","radX%.iradY%.iradZ%.i"%(int(radX*1e3),int(radY*1e3),int(radZ*1e3)),\
+		title="Different beam radii [mm]")
+	tbl_data = file_save.create_table(grp_simulation,"Data",Results)
+	tbl_descriptions = file_save.create_table(grp_simulation,"Descriptions",Descriptions)
+	data_save = tbl_data.row
+	descr_save = tbl_descriptions.row
+	
+	for num,ic in enumerate(inits):
+		print("Solving ",num," for rad ",radX)
+		solution_blue = odeint(diffeqs_blue, ic, t, args=(parameters_blue,),mxstep=10**8)
+		
+		data_save["x_init"] = solution_blue[0,0]
+		data_save["vx_init"] = solution_blue[0,1]
+		data_save["y_init"] = solution_blue[0,2]
+		data_save["vy_init"] = solution_blue[0,3]
+		data_save["z_init"] = solution_blue[0,4]
+		data_save["vz_init"] = solution_blue[0,5]
+		data_save["x_final"] = solution_blue[-1,0]
+		data_save["vx_final"] = solution_blue[-1,1]
+		data_save["y_final"] = solution_blue[-1,2]
+		data_save["vy_final"] = solution_blue[-1,3]
+		data_save["z_final"] = solution_blue[-1,4]
+		data_save["vz_final"] = solution_blue[-1,5]
+		data_save.append()
 
-    #tbl_inits = file_save.create_table(grp_simulation,"init"+str(counter),Coords_and_vel,"Initial conditions")
-    tbl_results = file_save.create_table(grp_simulation,str(counter),Coords_and_vel,"Results")
-    tbl_results.attrs.initialconds = "x: %.f, vx: %.f, y: %.f, vy: %.f, z: %.f, vz: %.f"%(inits[0],inits[1],inits[2],inits[3],inits[4],inits[5])
-    coords_and_vel = tbl_results.row
+	descr_save["detuning"] = detunings_blue[0]
+	descr_save["gradient"] = blueGradient
+	descr_save["init_position_away"] = init_rad
+	descr_save["PowerX"] = bluePowerX
+	descr_save["PowerY"] = bluePowerY
+	descr_save["PowerZ"] = bluePowerZ
+	descr_save["WaistRadX"] = radX
+	descr_save["WaistRadY"] = radY
+	descr_save["WaistRadZ"] = radZ
+	descr_save.append()
+
+	tbl_data.flush()
+	tbl_descriptions.flush()
+
+	file_save.close()
 
 
-    print("Solving for initial conditions %.i out of %.i"%(counter,len(initialconds_red)))
-    solution_red = odeint(diffeqs_red, inits, t, args=(parameters_red,),mxstep=10**8)
-    print("Saving data for initial conditions %.i out of %.i"%(counter,len(initialconds_red)))
-
-    for i in range(len(solution_red)):
-        coords_and_vel["time"] = t[i]
-        coords_and_vel["x_pos"] = solution_red[i,0]   
-        coords_and_vel["vx"] = solution_red[i,1] 
-        coords_and_vel["y_pos"] = solution_red[i,2] 
-        coords_and_vel["vy"] = solution_red[i,3] 
-        coords_and_vel["z_pos"] = solution_red[i,4] 
-        coords_and_vel["vz"] = solution_red[i,5] 
-        coords_and_vel.append()
-
-    tbl_results.flush()
-    del tbl_results
-    del solution_red
-    print("Initial conditions %.i out of %.i are done"%(counter,len(initialconds_red)))
-    counter += 1
-
-file_save.close()
 sys.exit(0)
 
 
-#---------------------------------------------------------
 
---
+++ plotterBlueMOT.py
@@ -0,0 +1,107 @@
+import tables
+import pandas as pd
+import matplotlib.pyplot as plt 
+import numpy as np 
+import sys
+#import matplotlib.animation as animation
+import itertools
+
+filename = "C:/Users/Oleksiy/Desktop/Code/PyRedMOT/resultsBlueMOT/pX15pY15pZ2grad55.hdf5"
+#filename="/Users/oleksiy/Desktop/PythonCode/Transversecooling/resultsTC/pow30mWspeed550det1.hdf5"
+datafile = tables.open_file(filename,"r")
+
+
+radiiMM = [2,7,12,17]
+nodes = ["radX%.iradY%.iradZ%.i/Data"%(radiusMM,radiusMM,radiusMM) for radiusMM in radiiMM]
+
+
+
+
+loaded_nodes = [pd.read_hdf(filename,key=node,mode="r") for node in nodes] # we have to give it a file 
+#name, the ndoe to read, and tell it to do it read-only
+
+
+
+"""
+We load the HDF5 which has beed read before as a DataFrame (using Pandas)
+
+We then check condition by condition to narrow down and select the data that we like 
+"""
+def distance_func(vec):
+    return np.sqrt(vec[0]**2+vec[1]**2+vec[2]**2)
+
+final_positions = []
+captured_number = []
+
+for index,u in enumerate(loaded_nodes):
+
+    final_positions.append(pd.DataFrame(u,columns=["x_final","y_final","z_final"]))
+
+for q in final_positions:
+    captured = q[abs(q["x_final"])<1e-4]
+    captured = captured[abs(captured["y_final"])<1e-4]
+    captured = captured[abs(captured["z_final"])<1e-4]
+    print(captured)
+    captured_number.append(len(captured))
+    
+print(captured_number)
+
+sys.exit(0)
+
+final_positions = np.array(final_positions)
+
+
+sys.exit(0)
+    
+
+    # success_data = full_data
+    # #success_data = success_data[success_data['init_x'] < 2.1e-3]
+    # #success_data = success_data[success_data['init_y'] < 1e-3]
+    # #success_data = success_data[success_data['init_vy'] < 1e-3]
+    # #success_data = success_data[success_data['init_vx'] > 2]
+    # #success_data = success_data[success_data['init_vx'] < 10]
+    # success_data = success_data[success_data['final_speed_xy'] <= 1.5]
+    # #print(np.sqrt(success_data['init_vx']**2+success_data['init_vx']**2))
+    
+    
+    # fail_data = full_data
+    # #fail_data = fail_data[fail_data['init_x'] < 2.1e-3]
+    # #success_data = success_data[success_data['init_y'] < 1e-3]
+    # #success_data = success_data[success_data['init_vy'] < 1e-3]
+    # #success_data = success_data[success_data['init_vx'] > 2]
+    # #success_data = success_data[success_data['init_vx'] < 10]
+    # fail_data = fail_data[fail_data['final_speed_xy'] > 1.5]
+    # #print(success_data)
+    
+    # fig = plt.figure()
+    # ax1 = fig.add_subplot(211)
+    
+    
+    # ax1.scatter(np.sqrt(success_data["init_vx"]**2 + success_data["init_vy"]**2),success_data["final_speed_xy"],c="g")
+    # ax1.set_title("a%.i b%.i"%ab_combinations[index])
+    # ax1.set_xlabel("Init speed [m/s]")
+    # ax1.set_ylabel("Final speed [m/s]")
+    
+    # ax2 = fig.add_subplot(212)
+    
+    # ax2.scatter(np.sqrt(fail_data["init_vx"]**2 + fail_data["init_vy"]**2),fail_data["final_speed_xy"],c="r")
+    # ax2.set_title("a%.i b%.i"%ab_combinations[index])
+    # ax2.set_xlabel("Init speed [m/s]")
+    # ax2.set_ylabel("Final speed [m/s]")
+    
+    # plt.show()
+    
+    # print("Captured: %.i"%len(success_data))
+    # print("Failed: %.i"%len(fail_data))
+    # print("Ratio: %.4f"%(len(success_data)/len(fail_data)))
+    
+
+
+"""
+
+
+Notes: 
+"index" apparently means rows and "columns" are as it sounds in Pandas
+
+"""
+
