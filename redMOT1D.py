2265ba74d4acd0f79ffe00ab9c986990dd221139
Oleksiy1990 <oleksiy1990@gmail.com>
2016-07-20 23:20:29 +0200

    First simulations for red done
    The cutoff was set a bit wrong, because it would make more sense to set
    the cutoff at beam radius, because that's where the beams in the other
    axes would end, even if the particular beam that we're using has a large
    Rayleigh range. In the next simulation, the cutoff even on the long axis
    should be set at the value of the Gaussian waist of that beam

--- redMOT1D.py
+++ redMOT1D.py
@@ -1,18 +1,18 @@
 import numpy as np 
 import scipy as sp 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import tables #this is PyTables to use HDF5 for Python 
-import itertools
+#import itertools
 import sys
 import os
 
-import multiprocessing as mpr 
-
+#import multiprocessing as mpr 
+from multiprocessing import Process, Lock
 
 
 from scipy.integrate import odeint 
-from numpy.random import random_sample, uniform
-from mpl_toolkits.mplot3d import Axes3D
+#from numpy.random import random_sample, uniform
+#from mpl_toolkits.mplot3d import Axes3D
 
 from pyConstants import *
 from SrConstants import * 
@@ -84,8 +84,8 @@ def simulation_func_red(init,params,t,tbl_results,tbl_simparameters,current_init
 
     tbl_simparameters.flush()
 
-    solution = odeint(diffeqs1D_red, init, t, args=(params,),mxstep=10**8)
-    
+    solution = odeint(diffeqs1D_red, init, t, args=(params,),full_output=False,mxstep=10**4) #it was 10**8 before
+    #print(solution[1])
     results["r_init"] = params[3]
     results["x_init"] = solution[0,0]
     results["vx_init"] = solution[0,1]
@@ -98,69 +98,69 @@ def simulation_func_red(init,params,t,tbl_results,tbl_simparameters,current_init
 
 if __name__ == '__main__': 
 
-    for num_redCombLines in [100,150,200,250]:
-        for redPower in [5e-3,8e-3,11e-3,14e-3]:
-            for redBeamRadius in [2e-3,7e-3,12e-3,17e-3]:
+    num_redCombLines = int(sys.argv[1]) #Give the number of comb lines as a command line argument 
+    for redPower in [7e-3,10e-3,13e-3]:
+        for redBeamRadius in [2e-3,7e-3,12e-3,17e-3]:
 
-                print("Doing beam rad %.i, power %.i, lines %.i"%(int(redBeamRadius*1e3),int(redPower*1e3),num_redCombLines))
+            print("Doing beam rad %.i, power %.i, lines %.i"%(int(redBeamRadius*1e3),int(redPower*1e3),num_redCombLines))
 
-                #num_redCombLines = 250
-                detunings_red = np.linspace(-2*np.pi*200*10**3,-2*np.pi*5*10**6,num_redCombLines)
-                redGradient = 1.15/100 # T/m
-                gravity = 1 #this is given only as 1 or 0 to determine if it's taken into account or not 
-
-
-                #redPower = 10e-3
-                #redBeamRadius = 17e-3 
-
-                params = [[redPower,redBeamRadius,redGradient,r,gravity,detunings_red] for r in np.linspace(0,17e-3,10)]
-                inits = [(x,vx) for x in np.linspace(-17e-3,0,10) for vx in np.linspace(-0.9,0.9,15)] #0.9 m/s is the max speed covering >95% atoms at 720 microK
-                print("There are %.i params"%len(params))
-                print("There are %.i inits"%len(inits))
-
-                tStop = 0.5
-                t = np.linspace(0., tStop, 10**4)
-
-                naming_tuple = (int(params[0][0]*1e3), # this gives the power in mW
-                    int(params[0][1]*1e3), # this gives the beam radius in mm 
-                    int(params[0][2]*1e4), # this gives the gradient, with the assumption of a comma after the first digit
-                    len(params[0][5])) # this gives the number of comb lines in broadband MOT
+            #num_redCombLines = 250
+            detunings_red = np.linspace(-2*np.pi*200*10**3,-2*np.pi*5*10**6,num_redCombLines)
+            redGradient = 1.15/100 # T/m
+            gravity = 1 #this is given only as 1 or 0 to determine if it's taken into account or not 
 
 
-                
-                filename_windows = "C:/Users/Oleksiy/Desktop/Code/PyRedMOT/resultsRedMOT/redMOT%.ilinesGravityOn.hdf5"%num_redCombLines
-                filename_mac = "/Users/oleksiy/Desktop/PythonCode/PyRedMOT/resultsRedMOT/redMOT%.ilines.hdf5"%num_redCombLines
+            #redPower = 10e-3
+            #redBeamRadius = 17e-3 
 
-                if bool(gravity):
-                    file_save = tables.open_file(filename_mac,mode="a",title= "Red MOT simulation, %.i comb"%num_redCombLines)
-                else:
-                    file_save = tables.open_file(filename_mac,mode="a",title= "Red MOT simulation, %.i comb"%num_redCombLines)
-                
+            params = [[redPower,redBeamRadius,redGradient,r,gravity,detunings_red] for r in np.linspace(0,17e-3,7)]
+            inits = [(x,vx) for x in np.linspace(-17e-3,0,10) for vx in np.linspace(-0.9,0.9,10)] #0.9 m/s is the max speed covering >95% atoms at 720 microK
+            print("There are %.i params"%len(params))
+            print("There are %.i inits"%len(inits))
 
-                simulation_groupname = "gradient%.iGcm"%int(redGradient*1e4)
+            tStop = 0.5
+            t = np.linspace(0., tStop, 10**4)
 
-                if not file_save.__contains__("/"+simulation_groupname):
-                    file_save.create_group("/",simulation_groupname)
+            naming_tuple = (int(params[0][0]*1e3), # this gives the power in mW
+                int(params[0][1]*1e3), # this gives the beam radius in mm 
+                int(params[0][2]*1e4), # this gives the gradient, with the assumption of a comma after the first digit
+                len(params[0][5])) # this gives the number of comb lines in broadband MOT
 
-                table_res_name = "Pow%.imWredRad%.immGrad%.iGcmlines%.i"%naming_tuple
-                table_simparams_name = "ParamsPow%.imWredRad%.immGrad%.iGcmlines%.i"%naming_tuple
-                array_detunigns_name = "ComblinesPow%.imWredRad%.immGrad%.iGcmlines%.i"%naming_tuple
 
-                if file_save.__contains__("/"+simulation_groupname+"/"+table_res_name):
-                    file_save.remove_node("/"+simulation_groupname+"/"+table_res_name,recursive=True)
-                if file_save.__contains__("/"+simulation_groupname+"/"+table_simparams_name):
-                    file_save.remove_node("/"+simulation_groupname+"/"+table_simparams_name,recursive=True)
-                if file_save.__contains__("/"+simulation_groupname+"/"+array_detunigns_name):
-                    file_save.remove_node("/"+simulation_groupname+"/"+array_detunigns_name,recursive=True)
-                
-                tbl_results = file_save.create_table("/gradient%.iGcm"%int(redGradient*1e4),table_res_name,Results,"Results") 
-                tbl_simparameters = file_save.create_table("/gradient%.iGcm"%int(redGradient*1e4),table_simparams_name,SimParameters,"Parameters")
-                
-                arr_detunings = file_save.create_array("/gradient%.iGcm"%int(redGradient*1e4),array_detunigns_name,params[0][5],"Comb lines")
-                  
-                
-                [simulation_func_red(init,param,t,tbl_results,tbl_simparameters,ni,np) for ni,init in enumerate(inits) for np,param in enumerate(params)]
-                file_save.close()
+            
+            filename_windows = "C:/Users/Oleksiy/Desktop/Code/PyRedMOT/resultsRedMOT/redMOT%.ilinesGravityOn.hdf5"%num_redCombLines
+            filename_mac = "/Users/oleksiy/Desktop/PythonCode/PyRedMOT/resultsRedMOT/redMOT%.ilines.hdf5"%num_redCombLines
+
+            if bool(gravity):
+                file_save = tables.open_file(filename_windows,mode="a",title= "Red MOT simulation, %.i comb"%num_redCombLines)
+            else:
+                file_save = tables.open_file(filename_windows,mode="a",title= "Red MOT simulation, %.i comb"%num_redCombLines)
+            
+
+            simulation_groupname = "gradient%.iGcm"%int(redGradient*1e4)
+
+            if not file_save.__contains__("/"+simulation_groupname):
+                file_save.create_group("/",simulation_groupname)
+
+            table_res_name = "Pow%.imWredRad%.immGrad%.iGcmlines%.i"%naming_tuple
+            table_simparams_name = "ParamsPow%.imWredRad%.immGrad%.iGcmlines%.i"%naming_tuple
+            array_detunigns_name = "ComblinesPow%.imWredRad%.immGrad%.iGcmlines%.i"%naming_tuple
+
+            if file_save.__contains__("/"+simulation_groupname+"/"+table_res_name):
+                file_save.remove_node("/"+simulation_groupname+"/"+table_res_name,recursive=True)
+            if file_save.__contains__("/"+simulation_groupname+"/"+table_simparams_name):
+                file_save.remove_node("/"+simulation_groupname+"/"+table_simparams_name,recursive=True)
+            if file_save.__contains__("/"+simulation_groupname+"/"+array_detunigns_name):
+                file_save.remove_node("/"+simulation_groupname+"/"+array_detunigns_name,recursive=True)
+            
+            tbl_results = file_save.create_table("/gradient%.iGcm"%int(redGradient*1e4),table_res_name,Results,"Results") 
+            tbl_simparameters = file_save.create_table("/gradient%.iGcm"%int(redGradient*1e4),table_simparams_name,SimParameters,"Parameters")
+            
+            arr_detunings = file_save.create_array("/gradient%.iGcm"%int(redGradient*1e4),array_detunigns_name,params[0][5],"Comb lines")
+              
+            
+            [simulation_func_red(init,param,t,tbl_results,tbl_simparameters,ni,np) for ni,init in enumerate(inits) for np,param in enumerate(params)]
+            file_save.close()
 
 
 
