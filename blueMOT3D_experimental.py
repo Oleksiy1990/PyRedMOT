f3fa8b72f25f9294e7753339841ae31b191da6dc
Oleksiy1990 <oleksiy1990@gmail.com>
2017-03-13 22:39:35 +0100

    Changed the code around a little bit to try the experimental situation
    Put up the

--- blueMOT3D_experimental.py
+++ blueMOT3D_experimental.py
@@ -14,10 +14,6 @@ from mpl_toolkits.mplot3d import Axes3D
 from pyConstants import *
 from SrConstants import * 
 
-"""
-General equations for the MOT force in 3D. This assumes the approximation of perfect polarization of
-the beams
-"""
 
 viewport_rad = 19e-3 #radius of the windows in meters (19mm, 38mm diam). This explicitly takes into account the fact that the beam can be clipped by the viewport
 
@@ -31,8 +27,8 @@ def MOT_force(k_laser,linewidth,sat_param,velocity,B_gradient,position,detunings
 
 
 def diffeqs_blue(variables,t,params): #We disregard gravity
-    (x,vx,y,vy,z,vz) = variables
-    (laserPowerX,laserPowerY,laserPowerZ,beamWaistRadX,beamWaistRadY,beamWaistRadZ,B_gradient,detunings_list) = params
+    (x,vx) = variables
+    (laserPowerX,beamWaistRadX,B_gradient,detunings_list) = params
 
 
     # This assumes circular beams!
@@ -72,10 +68,6 @@ blueGradient = 0.01*blueGradientGcm # T/m = 55 G/cm
 
 
 
-# parameters_blue = [bluePowerX,bluePowerY,bluePowerZ,blueRadX,blueRadY,blueRadZ,blueGradient,detunings_blue]
-
-
-
 tStop = 0.5
 t = np.linspace(0., tStop, 10**5)
 
@@ -105,89 +97,4 @@ parameters_blue = [bluePowerX,bluePowerY,bluePowerZ,blueRadX,blueRadY,blueRadZ,b
 odeint(diffeqs_blue, inits, t, args=(parameters_blue,),mxstep=10**8)
 
 sys.exit(0)
-
-
-class Results(tables.IsDescription):
-    x_init = tables.Float64Col()   
-    vx_init = tables.Float64Col()
-    y_init = tables.Float64Col()
-    vy_init = tables.Float64Col()
-    z_init = tables.Float64Col()
-    vz_init = tables.Float64Col()
-    x_final = tables.Float64Col()   
-    vx_final = tables.Float64Col()
-    y_final = tables.Float64Col()
-    vy_final = tables.Float64Col()
-    z_final = tables.Float64Col()
-    vz_final = tables.Float64Col()
-   
-
-class Descriptions(tables.IsDescription):
-    detuning = tables.Float64Col()
-    gradient = tables.Float64Col()
-    init_position_away = tables.Float64Col()
-    PowerX = tables.Float64Col()
-    PowerY = tables.Float64Col()
-    PowerZ = tables.Float64Col() 
-    WaistRadX = tables.Float64Col()
-    WaistRadY = tables.Float64Col()
-    WaistRadZ = tables.Float64Col()
-
-
-
-# grp_descriptions = file_save.create_group(grp_simulation,"Descriptions",\
-#     title="The input parameters of the simulation are given here, units are SI")
-
-radiiXY = np.array([5,8,11,14,17])*1e-3
-for radX in radiiXY:
-	radY = radZ = radX
-	parameters_blue = [bluePowerX,bluePowerY,bluePowerZ,radX,radY,radZ,blueGradient,detunings_blue]
-
-	file_save = tables.open_file("resultsBlueMOT/pX%.ipY%.ipZ%.igrad%.i.hdf5"%(int(bluePowerX*1e3),int(bluePowerY*1e3),int(bluePowerZ*1e3),\
-		blueGradientGcm),mode="a",title= "Blue MOT simulation, detuning -1*gamma")
-	grp_simulation = file_save.create_group("/","radX%.iradY%.iradZ%.i"%(int(radX*1e3),int(radY*1e3),int(radZ*1e3)),\
-		title="Different beam radii [mm]")
-	tbl_data = file_save.create_table(grp_simulation,"Data",Results)
-	tbl_descriptions = file_save.create_table(grp_simulation,"Descriptions",Descriptions)
-	data_save = tbl_data.row
-	descr_save = tbl_descriptions.row
-	
-	for num,ic in enumerate(inits):
-		print("Solving ",num," for rad ",radX)
-		solution_blue = odeint(diffeqs_blue, ic, t, args=(parameters_blue,),mxstep=10**8)
-		
-		data_save["x_init"] = solution_blue[0,0]
-		data_save["vx_init"] = solution_blue[0,1]
-		data_save["y_init"] = solution_blue[0,2]
-		data_save["vy_init"] = solution_blue[0,3]
-		data_save["z_init"] = solution_blue[0,4]
-		data_save["vz_init"] = solution_blue[0,5]
-		data_save["x_final"] = solution_blue[-1,0]
-		data_save["vx_final"] = solution_blue[-1,1]
-		data_save["y_final"] = solution_blue[-1,2]
-		data_save["vy_final"] = solution_blue[-1,3]
-		data_save["z_final"] = solution_blue[-1,4]
-		data_save["vz_final"] = solution_blue[-1,5]
-		data_save.append()
-
-	descr_save["detuning"] = detunings_blue[0]
-	descr_save["gradient"] = blueGradient
-	descr_save["init_position_away"] = init_rad
-	descr_save["PowerX"] = bluePowerX
-	descr_save["PowerY"] = bluePowerY
-	descr_save["PowerZ"] = bluePowerZ
-	descr_save["WaistRadX"] = radX
-	descr_save["WaistRadY"] = radY
-	descr_save["WaistRadZ"] = radZ
-	descr_save.append()
-
-	tbl_data.flush()
-	tbl_descriptions.flush()
-
-	file_save.close()
-
-
-sys.exit(0)
-
-
 
