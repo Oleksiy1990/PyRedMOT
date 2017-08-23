d4d25f71fac0bc541b1556fcddf4df62babf77c2
Oleksiy1990 <oleksiy1990@gmail.com>
2017-08-23 19:58:31 +0200

    Revert "Changed the redMOT3D file"
    This reverts commit d280355142bce9a53efe05ee7a3eaebb155c3209.

--- pyConstants.py
+++ pyConstants.py
@@ -5,5 +5,3 @@ hbar = codata.value("Planck constant over 2 pi")
 c0 = codata.value("speed of light in vacuum")
 muBohr = codata.value("Bohr magneton")
 amu = codata.value("atomic mass constant")
-
-g_Earth = 9.81 # m/s
